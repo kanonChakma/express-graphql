b54d3000376a1920dfb9ea22068aa556affe7330
kanonChakma <kanon122chakma@gmail.com>
2023-09-27 19:23:17 +0600

    add product model

---
+++ src/schema/product.schema.ts
@@ -0,0 +1,34 @@
+import { getModelForClass, prop, Ref } from "@typegoose/typegoose";
+import { customAlphabet } from "nanoid";
+import { Field, ObjectType } from "type-graphql";
+import { User } from "./user.schema";
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz123456789", 10);
+
+@ObjectType()
+export class Product {
+  @Field(() => String)
+  _id: string;
+
+  @Field(() => String)
+  @prop({ required: true })
+  name: string;
+
+  @Field(() => String)
+  @prop({ required: true, ref: () => User })
+  user: Ref<User>;
+
+  @Field(() => String)
+  @prop({ required: true })
+  description: string;
+
+  @Field(() => String)
+  @prop({ required: true })
+  price: string;
+
+  @Field(() => String)
+  @prop({ required: true, default: () => `product_${nanoid()}, unique: true}` })
+  productId: string;
+}
+
+export const ProductModel = getModelForClass<typeof Product>(Product);
